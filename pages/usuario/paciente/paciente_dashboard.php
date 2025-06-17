5a7c35eb27a063f0a1027de40c8d241734e19d10
Fran <45077882+fran813@users.noreply.github.com>
2025-06-18 00:42:05 +0200

    Update paciente_dashboard.php

--- pages/usuario/paciente/paciente_dashboard.php
+++ pages/usuario/paciente/paciente_dashboard.php
@@ -5,7 +5,7 @@
     <div class="bg-white bg-opacity-50 p-10 rounded text-white text-center">
         <h2 class="text-4xl sm:text-5xl font-bold mb-4 text-purple-800">Recupera tu bienestar</h2>
         <p class="text-lg sm:text-xl mb-6 text-purple-600">Atención personalizada y profesional para ayudarte a sentirte mejor cada día.</p>
-        <a href="/reservas_proyecto/pages/usuario/paciente/citas/crear_cita.php" class="bg-purple-500 hover:bg-purple-600 text-white py-3 px-6 rounded-full shadow-lg transition">
+        <a href="/pages/usuario/paciente/citas/crear_cita.php" class="bg-purple-500 hover:bg-purple-600 text-white py-3 px-6 rounded-full shadow-lg transition">
         Reserva tu cita ahora
         </a>
     </div>
