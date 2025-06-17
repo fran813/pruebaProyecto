dbd58285b2dbb1604eb671a7c0dfd8c81689556f
Fran <45077882+fran813@users.noreply.github.com>
2025-06-18 00:53:35 +0200

    Update cambiar_contrasena.php

--- pages/usuario/paciente/perfil/cambiar_contrasena.php
+++ pages/usuario/paciente/perfil/cambiar_contrasena.php
@@ -5,7 +5,7 @@ include('../../../../includes/logger.php');
 
 //Verificacion de seguridad, existe sesion 
 if (!isset($_SESSION['user_id']) || $_SESSION['rol'] != 'paciente') {
-    header('Location: /reservas_proyecto/pages/public/login.php');
+    header('Location: /pages/public/login.php');
     exit();
 }
 
