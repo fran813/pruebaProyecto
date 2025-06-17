4427e7cbf089c6d4288b18329f74ab3ae73e523a
Fran <45077882+fran813@users.noreply.github.com>
2025-06-17 20:42:47 +0200

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ include 'includes/db.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio | Reservas Fisio</title>
-    <link href="/reservas_proyecto/dist/output.css" rel="stylesheet">
+    <link href="dist/output.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
 </head>
 <body class="flex flex-col min-h-screen bg-gray-100">
